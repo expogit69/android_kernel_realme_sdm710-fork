a192057982bc35dc7d6ef6a4b4aee043fbf0eac6
Sorayukii <sorayukii69@gmail.com>
2025-10-04 23:57:59 +0700

    fs: Integrate SukiSU tracepoint hooks-v1.1
    reference: https://github.com/SukiSU-Ultra/SukiSU_patch/blob/main/hooks/sukisu_tracepoint_hooks_v1.1.patch
    
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>
    Signed-off-by: iabdollah40 <iabdollah40@gmail.com>

--- fs/stat.c
+++ fs/stat.c
@@ -18,6 +18,19 @@
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
 
+#if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
+#include <../drivers/kernelsu/ksu_trace.h>
+#endif
+
+/**
+ * generic_fillattr - Fill in the basic attributes from the inode struct
+ * @inode: Inode to use as the source
+ * @stat: Where to fill in the attributes
+ *
+ * Fill in the basic attributes in the kstat structure from data that's to be
+ * found on the VFS inode structure.  This is the default if no getattr inode
+ * operation is supplied.
+ */
 void generic_fillattr(struct inode *inode, struct kstat *stat)
 {
 	stat->dev = inode->i_sb->s_dev;
@@ -294,6 +307,10 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
+#if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
+	trace_ksu_trace_stat_hook(&dfd, &filename, &flag);
+#endif
+
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
 		return error;
@@ -435,6 +452,10 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 {
 	struct kstat stat;
 	int error;
+
+#if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
+	trace_ksu_trace_stat_hook(&dfd, &filename, &flag); /* 32-bit su support */
+#endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
